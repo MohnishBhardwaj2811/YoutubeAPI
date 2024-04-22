3935ca07d831463a20a2a23ef6cea93ff988f62b
MohnishBhardwaj2811 <160467618+MohnishBhardwaj2811@users.noreply.github.com>
2024-04-22 10:47:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-def index():
-    return """
+from flask import Flask, render_template, request,jsonify,redirect, redirect, url_for, session
+import json
+import requests
+from bs4 import BeautifulSoup
+import re
+from pytube import YouTube
+#################################################################################################################################
+def index_html(Data):
+    index_html = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,25 +20,30 @@ def index():
             padding: 0;
             background-color: #f0f0f0;
         }
+
         header {
             background-color: #333;
             color: #fff;
             text-align: center;
             padding: 10px;
         }
+
         .search-container {
             text-align: center;
-            margin: 20px auto;
+            margin: 20px auto; /* Center the search container */
         }
+
         .search-form {
             display: inline-block;
             padding: 20px;
             border-radius: 10px;
             background-color: #ddd;
         }
+
         .search-form input[type="text"] {
             padding: 10px;
         }
+
         .search-form button {
             padding: 5px 10px;
             background-color: #333;
@@ -39,31 +51,37 @@ def index():
             border: none;
             cursor: pointer;
         }
-        .item {
+
+        .item.horizontal-item {
             display: flex;
             align-items: center;
             padding: 10px;
             background-color: #fff;
             border-radius: 5px;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            margin-bottom: 20px;
+            margin-bottom: 20px; /* Add margin between items */
         }
-        .thumbnail {
-            width: 200px;
-            height: auto;
+
+        .item.horizontal-item .thumbnail {
+            width: 200px; /* Adjust the width as needed */
+            height: auto; /* Maintain aspect ratio */
             margin-right: 10px;
             border-radius: 5px;
         }
-        .data {
-            flex: 1;
+
+        .item.horizontal-item .data {
+            flex: 1; /* Take remaining space */
         }
-        .data h3 {
+
+        .item.horizontal-item h3 {
             margin-top: 0;
         }
-        .data p {
+
+        .item.horizontal-item p {
             margin: 5px 0;
         }
-        .data a {
+
+        .item.horizontal-item a {
             display: block;
             margin-top: 10px;
             text-decoration: none;
@@ -71,6 +89,14 @@ def index():
             font-weight: bold;
         }
     </style>
+    <script>
+    function sendData(data) {
+        const url = 'Listen';
+        const query = `?videoId=${encodeURIComponent(data.videoId)}&thumbnail=${encodeURIComponent(data.thumbnail)}&title=${encodeURIComponent(data.title)}&publishedTimeText=${encodeURIComponent(data.publishedTimeText)}&views=${encodeURIComponent(data.views)}&Length=${encodeURIComponent(data.Length)}`;
+        window.location.href = url + query;
+    }
+    </script>
+
 </head>
 <body>
 
@@ -79,40 +105,27 @@ def index():
 </header>
 
 <div class="search-container">
-    <form class="search-form">
+    <form class="search-form" action="/" method="post">
         <input type="text" name="query" placeholder="Search...">
-        <button type="button">Search</button>
+        <button type="submit">Search</button>
     </form>
 </div>
 
-<div class="items">
-    <!-- Sample Item -->
-    <div class="item">
-                <img src="https://i.ytimg.com/vi/-4JV4hur1PM/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBZowGWFFb399CHtSHm3PopKR9LQA" alt="Item 2 Image" class="thumbnail">
-                <div class="data">
-                    <h3>Tera Hua Video Song With Lyrics | Atif Aslam | Loveyatri | Aayush Sharma | Warina Hussain |Tanishk B</h3>
-                    <p>5 years ago</p>
-                    <p>146,671,148 views</p>
-                    <p>3:52</p>
-                    <a href="#" onclick="sendData({'videoId': '-4JV4hur1PM', 'thumbnail': 'https://i.ytimg.com/vi/-4JV4hur1PM/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBZowGWFFb399CHtSHm3PopKR9LQA', 'title': 'Tera Hua Video Song With Lyrics | Atif Aslam | Loveyatri | Aayush Sharma | Warina Hussain |Tanishk B', 'publishedTimeText': '5 years ago', 'views': '146,671,148 views', 'Length': '3:52'})">Details</a>
-                </div>
-            </div>
-</div>
+<div class="items">""" + f""" 
 
-<script>
-function sendData(data) {
-    const url = 'Listen';
-    const query = `?videoId=${encodeURIComponent(data.videoId)}&thumbnail=${encodeURIComponent(data.thumbnail)}&title=${encodeURIComponent(data.title)}&publishedTimeText=${encodeURIComponent(data.publishedTimeText)}&views=${encodeURIComponent(data.views)}&Length=${encodeURIComponent(data.Length)}`;
-    window.location.href = url + query;
-}
-</script>
+{Data}
+
+    
+</div>
 
 </body>
 </html>
-"""
 
-def listen():
-    return """
+"""
+    return index_html
+
+def Listen_html(Data):
+    html = """
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -129,6 +142,7 @@ def listen():
             height: 100vh;
             margin: 0;
         }
+
         .audio-player {
             background-color: #fff;
             border-radius: 10px;
@@ -137,21 +151,25 @@ def listen():
             width: 300px;
             text-align: center;
         }
+
         .thumbnail {
             width: 100%;
             border-radius: 10px;
             margin-bottom: 10px;
         }
+
         audio {
             width: 100%;
             outline: none;
             margin-bottom: 20px;
         }
+
         .controls {
             display: flex;
             justify-content: center;
             align-items: center;
         }
+
         .control-button {
             background-color: #007bff;
             border: none;
@@ -160,37 +178,43 @@ def listen():
             border-radius: 5px;
             margin: 0 10px;
             cursor: pointer;
+            transition: background-color 0.3s;
         }
+
         .control-button:hover {
             background-color: #0056b3;
         }
+
         .volume {
             display: flex;
             align-items: center;
             justify-content: center;
             margin-top: 10px;
         }
+
         .volume input[type="range"] {
             width: 100px;
             margin: 0 10px;
         }
+
         .title {
             font-weight: bold;
             margin-bottom: 10px;
         }
+
         .view-count {
             color: #666;
             font-size: 0.8em;
         }
     </style>
-</head> 
+</head> """ + f"""
 <body>
     <div class="audio-player">
-        <img class="thumbnail" src="" alt="Thumbnail">
-        <h2 class="title"></h2>
-        <span class="view-count"></span>
+        <img class="thumbnail" src="{Data["thumbnail"]}" alt="Thumbnail">
+        <h2 class="title">{Data['title']}</h2>
+        <span class="view-count">{Data['views']}</span>
         <audio controls>
-            <source src="" type="audio/mpeg">
+            <source src="{Data['audio_url']}" type="audio/mpeg">
             Your browser does not support the audio element.
         </audio>
         <div class="controls">
@@ -205,27 +229,9 @@ def listen():
     </div>
 
     <script>
-        window.onload = function() {
-            const urlParams = new URLSearchParams(window.location.search);
-            const videoId = urlParams.get('videoId');
-            const thumbnail = urlParams.get('thumbnail');
-            const title = urlParams.get('title');
-            const views = urlParams.get('views');
-            const publishedTimeText = urlParams.get('publishedTimeText');
-            const length = urlParams.get('Length');
-
-            // Set the audio player details
-            document.querySelector('.thumbnail').src = thumbnail;
-            document.querySelector('.title').textContent = title;
-            document.querySelector('.view-count').textContent = `${views} views`;
-            
-            // Example for setting the audio source, change as necessary
-            document.querySelector('audio source').src = `https://example.com/audio/${videoId}.mp3`;
-            document.querySelector('audio').load();
-        };
+        const audio = document.querySelector('audio'); """ + """
 
         function togglePlayPause() {
-            const audio = document.querySelector('audio');
             if (audio.paused) {
                 audio.play();
             } else {
@@ -234,31 +240,146 @@ def listen():
         }
 
         function skip(time) {
-            const audio = document.querySelector('audio');
             audio.currentTime += time;
         }
 
         function changeVolume(volume) {
-            const audio = document.querySelector('audio');
             audio.volume = volume;
         }
     </script>
 </body>
 </html>
 
+
+
 """
-from flask import Flask, render_template, request,jsonify,redirect, redirect, url_for, session
+    return html
+
+##############################################################################################################################################
+
+def Scrap_youtube_search(url):
+    try :
+        # URL of the webpage
+        url = url
+        # Send a GET request to the URL
+        response = requests.get(url)
+        # Parse the HTML content
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Find script tags containing the variable ytInitialData
+        script_tags = soup.find_all('script', text=re.compile(r'ytInitialData'))
+        # Extract the content of ytInitialData variable
+        yt_initial_data = None
+        for script in script_tags:
+            # Search for ytInitialData assignment using regular expression
+            match = re.search(r'var ytInitialData = ({.*?});', script.text)
+            if match:
+                # Extract the content of ytInitialData
+                yt_initial_data = match.group(1)
+                break
+        return json.loads(yt_initial_data)
+    except:
+        print("Something Cause Error ! Failed Process !")
+
+def Data_HTML(search_query , typo = 0):
+    # Print the extracted ytInitialData
+    url = "https://www.youtube.com/results?search_query=" + search_query
+
+    yt_initial_data = Scrap_youtube_search(url)
+
+    html = ""
+    json_data = {}
+    for index in range(0,len(yt_initial_data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"])):
+        try:
+            Data = yt_initial_data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"][index]["videoRenderer"]
+            
+            
+            json_data["Data" + str(index)] = {"videoId"   : Data["videoId"] ,
+            "thumbnail" : Data['thumbnail']['thumbnails'][-1]['url'] ,
+            "title"     : Data['title']["runs"][0]["text"] ,
+            "publishedTimeText" : Data['publishedTimeText']['simpleText'],
+            "views"     : Data['viewCountText']['simpleText'] ,
+            "Length"    : Data['lengthText']["simpleText"] } 
+            
+            Value = json_data["Data" + str(index)]
+            html += f"""
+            <div class="item horizontal-item">
+                <img src="{Data['thumbnail']['thumbnails'][-1]['url']}" alt="Item 2 Image" class="thumbnail">
+                <div class="data">
+                    <h3>{Data['title']["runs"][0]["text"]}</h3>
+                    <p>{Data['publishedTimeText']['simpleText']}</p>
+                    <p>{Data['viewCountText']['simpleText']}</p>
+                    <p>{Data['lengthText']["simpleText"]}</p>
+                    <a href="#" onclick="sendData({Value})">Details</a>
+                </div>
+            </div> """
+
+
+        except:
+            #print("Nothing! at ", index)
+            pass
+    if typo == 0 : return html 
+    else: return json_data
+
+
+
+def extract_audio_url(youtube_url):
+    youtube_url = "https://www.youtube.com/watch?v=" + youtube_url
+    try:
+        # Create a YouTube object
+        yt = YouTube(youtube_url)
+
+        # Get the highest quality audio stream directly
+        audio_stream = yt.streams.get_audio_only()
+
+        # Get the URL of the highest quality audio stream
+        audio_url = audio_stream.url if audio_stream else None
+
+        return audio_url
+    except Exception as e:
+        print("Error:", e)
+        return "404"
+
+
+##########################################################################################################################################################
+
 app = Flask(__name__)
 
 # Define a route for handling the search request
 @app.route('/', methods=['GET', 'POST'])
 def search():
-    return index()
-    
+    global q_data
+    if request.method == 'POST':
+        try :
+            query = request.form['query']
+            print("Search Query:", query)
+            query = query.replace(" ", "+")
+            return index_html( Data_HTML(query , 0 ))
+        except:
+            data = request.get_json()
+            data['audio_url'] = extract_audio_url(data['videoId'])
+            print("Data Processed:\n", Listen_html(data))
+            return "request made"
+    else:
+        
+        return index_html("")
+
 @app.route('/Listen', methods=['GET', 'POST'])
-def search2():
-    video_id = request.args.get('videoId', 'DefaultVideoId')
-    thumbnail = request.args.get('thumbnail', 'DefaultThumbnail')
-    return video_id + ":" + thumbnail
+def Listen():
+    data = {
+            'videoId'       : request.args.get('videoId', 'No video ID provided'),
+            'thumbnail'      : request.args.get('thumbnail', 'No thumbnail provided'),
+            'title'          : request.args.get('title', 'No title provided'),
+            'publishedTimeText' : request.args.get('publishedTimeText', 'No published time provided'),
+            'views'         : request.args.get('views', 'No views provided'),
+            'Length'         : request.args.get('Length', 'No length provided') 
+            }
+    data['audio_url'] = extract_audio_url(data['videoId'])
+
+    return Listen_html(data)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
 
 
